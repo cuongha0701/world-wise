550f484b27ead84e195ef196429bd6b8ba9c1785
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-07 20:27:18 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import { RouterProvider } from 'react-router-dom';
-import { routes } from './Routes/routes.jsx';
+import { routes } from './routes/routes.jsx';
 
 // import App from './App';
 
